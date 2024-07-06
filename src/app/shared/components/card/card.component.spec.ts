95213179935852308badbb97ec74fd28b8b12c0e
Danilo Teixeira <danilo.rt@outlook.com>
2024-07-06 01:51:07 -0300

    test: create firsts tests

--- src/app/shared/components/card/card.component.spec.ts
+++ src/app/shared/components/card/card.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
+import { Product } from '../../../types/product.inteface';
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
@@ -8,10 +10,9 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardComponent]
-    })
-    .compileComponents();
-    
+      imports: [CardComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +20,49 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the card content', () => {
+    const product: Product = {
+      id: 1,
+      title: 'Iphone',
+      description: 'smartphone Apple',
+      image: 'src/assets/images/iphone.png',
+      price: '1000',
+      category: 'smartphone',
+    };
+
+    component.product = product;
+    fixture.detectChanges();
+
+    const title = fixture.debugElement.query(By.css('h2')).nativeElement;
+    const description = fixture.debugElement.query(By.css('p')).nativeElement;
+    const image = fixture.debugElement.query(By.css('img')).nativeElement;
+    const price = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+    expect(title.textContent).toContain(product.title);
+    expect(description.textContent).toContain(product.description);
+    expect(image.src).toContain(product.image);
+    expect(price.textContent).toContain(product.price);
+  });
+
+  it('should show edit and delete button if isManageable is true', () => {
+    component.isManageable = true;
+    fixture.detectChanges();
+
+    const buttonsContainer = fixture.debugElement.query(
+      By.css('span')
+    ).nativeElement;
+
+    expect(buttonsContainer).toBeTruthy();
+  });
+
+  it('should hide edit and delete button if isMabageable is false', () => {
+    component.isManageable = false;
+    fixture.detectChanges();
+
+    const buttonsContainer = fixture.debugElement.query(By.css('span'));
+
+    expect(buttonsContainer).toBeFalsy();
   });
 });
